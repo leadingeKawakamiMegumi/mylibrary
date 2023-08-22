2cb86b5035441267c04eaa5577d982bd7f5361e7
leadingeKawakamiMegumi <65448588+leadingeKawakamiMegumi@users.noreply.github.com>
2023-08-22 11:55:32 +0900

    Add files via upload

---
+++ mylibrary/__init__.py
@@ -0,0 +1,1 @@
+from mylibrary.mylibrary import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "mylibrary",
+    version = "20230822",
+    packages=find_packages()
+)
