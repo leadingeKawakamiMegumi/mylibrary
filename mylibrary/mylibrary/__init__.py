827f135dbae7384c5cbf871753b1b2ed727679f5
leadingeKawakamiMegumi <65448588+leadingeKawakamiMegumi@users.noreply.github.com>
2023-08-22 11:54:49 +0900

    Delete mylibrary directory

--- mylibrary/mylibrary/__init__.py
+++
@@ -1,1 +0,0 @@
-from mylibrary.mylibrary import *
--- mylibrary/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name = "mylibrary",
-    version = "20230822",
-    packages=find_packages()
-)
